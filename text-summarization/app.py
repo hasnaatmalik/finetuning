ce74ced41731ffa02bdc97577a75886cdb40f47d
hasnaatmalik <hasnaatmalik2003@gmail.com>
2025-11-03 14:37:37 +0500

    Fine Tuned T5 for text summarization w gradio interface

---
+++ text-summarization/app.py
@@ -0,0 +1,34 @@
+import gradio as gr
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+def summarize_text(text, model, tokenizer, max_length=512, num_beams=5):
+    # Preprocess the text
+    inputs = tokenizer.encode(
+        "summarize: " + text,
+        return_tensors='pt',
+        max_length=max_length,
+        truncation=True
+    )
+
+    # Generate the summary
+    summary_ids = model.generate(
+        inputs,
+        max_length=50,
+        num_beams=num_beams,
+    )
+
+    # Decode and return the summary
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+
+model_path = 'results_t5base/checkpoint-4450'
+model = T5ForConditionalGeneration.from_pretrained(model_path)
+tokenizer = T5Tokenizer.from_pretrained('results_t5base')
+
+interface = gr.interface(
+    fn = summarize_text,
+    inputs = gr.Textbox(lines = 10, placeholder="Enter Your Text Here..", label='Input Text'),
+    outputs = gr.Textbox(label='Summarized Text'),
+    title = "Text Summarizer using T5"
+)
+interface.launch()
